8b3d6f0fe8875e70c102e3cc61f602c0f874e5a4
jeevanpuhan91 <jeevankumarpuhan91@gmail.com>
2020-01-14 14:02:38 +0530

    Updated BankApp.java

--- CSW2/src/csw2/chapter1/bankApp.java
+++ CSW2/src/csw2/chapter1/bankApp.java
@@ -2,9 +2,9 @@ package csw2.chapter1;
 import java.util.Scanner;
 
 class Account{
-	String name;
-	int accNo;
-	double balance;
+	private String name;
+	private int accNo;
+	private double balance;
 
 	public Account(String name, int accNo, double balance){
 		this.name = name;
@@ -13,11 +13,22 @@ class Account{
 	}
 	
 	public void deposit(double amount){
+		
 		balance += amount;
 	}
 	
 	public void withdraw(double amount){
 		balance -= amount;
+	}
+	
+	public void display(){
+		System.out.println("Balance: "+balance);
+	}
+	
+	public void display_details(){
+		System.out.println("Name: "+name);
+		System.out.println("Account No: "+accNo);
+		System.out.println("Balance: "+balance);
 	}
 	
 }
@@ -63,13 +74,11 @@ public class bankApp {
 				break;
 				
 				case 3:
-				System.out.println("Balance: "+obj.balance);
+				obj.display();
 				break;
 				
 				case 4:
-				System.out.println("Name: "+obj.name);
-				System.out.println("Account No: "+obj.accNo);
-				System.out.println("Balance: "+obj.balance);
+				obj.display_details();
 				break;
 				
 				default:
