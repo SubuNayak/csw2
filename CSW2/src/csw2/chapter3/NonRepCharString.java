039903b0c12d67390676d8cfad1f026aa216de07
jeevanpuhan91 <jeevankumarpuhan91@gmail.com>
2020-01-25 11:31:14 +0530

    Added new string programs in ch-3

---
+++ CSW2/src/csw2/chapter3/NonRepCharString.java
@@ -0,0 +1,38 @@
+package csw2.chapter3;
+import java.util.*;
+
+public class NonRepCharString {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String s = sc.next();
+		s += sc.nextLine();
+		int count=230;
+		System.out.print("The first non-repeating character: ");
+		int i, j;
+		for(i = 0; i<s.length(); i++)
+		{
+			count = 0;
+			if(s.charAt(i)!=' ')
+			{
+			for(j=0; j<s.length(); j++)
+			{
+					if(s.charAt(i)==s.charAt(j))
+					{
+						count++;
+					}
+			}
+			
+			if(count == 1) 
+			{
+				System.out.println(s.charAt(i));
+				break;
+			}
+		
+		
+	}
+		}
+	}
+	}
