40109c3c176f19ba7d8438ab968e8ef486cc66d4
jeevanpuhan91 <jeevankumarpuhan91@gmail.com>
2020-02-18 13:14:12 +0530

    Added new programs to ch-4

--- CSW2/src/csw2/chapter4/StrMaxValue.java
+++ CSW2/src/csw2/chapter4/StrMaxValue.java
@@ -1,40 +1,36 @@
 package csw2.chapter4;
-import java.io.*;
-import java.util.regex.Matcher;
+import java.util.Scanner;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
-public class StrMaxDigit {
+public class StrMaxValue {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-		BufferedReader obj = new BufferedReader(new FileReader(args[0]));
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String s = sc.next();
+		s += sc.nextLine();
 		
 		String text = "\\d+";
-		String line;
 		int count = 0;
 		
 		Pattern p = Pattern.compile(text);
+		Matcher m = p.matcher(s);
+		
 		int max = 0;
-		while((line = obj.readLine()) != null) {
-			Matcher m = p.matcher(line);
+		while(m.find()) {
 			
-			if(m.find())
-			{
 			System.out.println(Integer.parseInt(m.group(0)));
 			int num = Integer.parseInt(m.group(0));
 			if (num>max)
 				{max = num;}
 			count++;
-			}
+			
 		}
 		
 		System.out.println("count = "+count);
 		System.out.println("Max = "+max);
-		
-		
-		
-	}
-	
-	
 
+}
 }
